c6b6e0333beb77d875ea77ed757160a1d36aa37f
yunkai111 <a281413@163.com>
2026-06-04 21:07:02 +0800

    fix: monologue page force-dynamic to show edits

--- app/monologue/page.jsx
+++ app/monologue/page.jsx
@@ -3,6 +3,8 @@ import remarkGfm from 'remark-gfm'
 import { BlogShell } from '@/components/blog-shell'
 import { getMonologue } from '@/lib/site-content'
 
+export const dynamic = 'force-dynamic'
+
 export default async function MonologueRoute() {
   const monologue = await getMonologue()
 
